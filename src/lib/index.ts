c3755e6275ecb471dc7adc55a173d212c6a60852
Adam Kovar <kovaradam1@gmail.com>
2021-09-26 19:22:09 +0200

    :recycle: separate shadows from react store

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,14 @@
-import { LightOriginType } from '../store';
-import { createShadowOffset } from '../use-light-origin';
+import { LightOriginType, Position } from '../store';
 
 type OriginUpdater = (origin: LightOriginType[]) => LightOriginType[];
+
+export const addShadow: typeof Store.subscribe = (element) => {
+  return Store.subscribe(element);
+};
+
+export const setOrigins: typeof Store.setOrigins = (origins) => {
+  return Store.setOrigins(origins);
+};
 
 class Store {
   static lightOrigins: LightOriginType[] = [];
@@ -42,10 +49,47 @@ class Store {
   };
 }
 
-export const addShadow: typeof Store.subscribe = (element) => {
-  return Store.subscribe(element);
-};
+function createShadowOffset(origin: LightOriginType, element: HTMLElement): string {
+  const elementRect = element.getBoundingClientRect();
+  const shadowDirection = getShadowDirection(origin, elementRect);
 
-export const setOrigins: typeof Store.setOrigins = (origins) => {
-  return Store.setOrigins(origins);
-};
+  return `${shadowDirection[0]}px ${shadowDirection[1]}px`;
+}
+
+type Direction = Position;
+
+function getShadowDirection(origin: LightOriginType, elementRect: DOMRect): Direction {
+  const { top, right, bottom, left } = elementRect;
+  const [centerLeft, centerTop] = [left + (right - left) / 2, top + (bottom - top) / 2];
+  const [originLeft, originTop] = origin.position;
+
+  const [shadowBorderLeft, shadowBorderTop] = ((): Position => {
+    if (originLeft <= centerLeft) {
+      if (originTop <= centerTop) {
+        return [right, bottom];
+      } else {
+        return [right, top];
+      }
+    } else {
+      if (originTop <= centerTop) {
+        return [left, bottom];
+      } else {
+        return [left, top];
+      }
+    }
+  })();
+
+  const elementHeight = 1;
+  const heightDiff = origin.height >= elementHeight ? origin.height - elementHeight : 0;
+  const [ratioLeft, ratioTop] = [
+    (shadowBorderLeft - originLeft) / heightDiff,
+    (shadowBorderTop - originTop) / heightDiff,
+  ];
+
+  const area: ReturnType<typeof getShadowDirection> = [
+    ratioLeft * elementHeight,
+    ratioTop * elementHeight,
+  ];
+
+  return area;
+}
--- src/use-light-origin.ts
+++ src/use-light-origin.ts
@@ -3,7 +3,6 @@ import { RefObject } from 'preact';
 import { useEffect, useRef } from 'react';
 
 import { addShadow } from './lib';
-import { LightOriginType, Position } from './store';
 
 export function useLightOriginRef<T extends HTMLElement>(): RefObject<T> {
   const elementRef = useRef<T>(null);
@@ -21,52 +20,4 @@ export function useLightOrigin<T extends HTMLElement>(elementRef: RefObject<T>):
     const unsub = addShadow(element);
     return unsub;
   }, [elementRef]);
-}
-
-export function createShadowOffset(
-  origin: LightOriginType,
-  element: HTMLElement,
-): string {
-  const elementRect = element.getBoundingClientRect();
-  const shadowDirection = getShadowDirection(origin, elementRect);
-
-  return `${shadowDirection[0]}px ${shadowDirection[1]}px`;
-}
-
-type Direction = Position;
-
-function getShadowDirection(origin: LightOriginType, elementRect: DOMRect): Direction {
-  const { top, right, bottom, left } = elementRect;
-  const [centerLeft, centerTop] = [left + (right - left) / 2, top + (bottom - top) / 2];
-  const [originLeft, originTop] = origin.position;
-
-  const [shadowBorderLeft, shadowBorderTop] = ((): Position => {
-    if (originLeft <= centerLeft) {
-      if (originTop <= centerTop) {
-        return [right, bottom];
-      } else {
-        return [right, top];
-      }
-    } else {
-      if (originTop <= centerTop) {
-        return [left, bottom];
-      } else {
-        return [left, top];
-      }
-    }
-  })();
-
-  const elementHeight = 1;
-  const heightDiff = origin.height >= elementHeight ? origin.height - elementHeight : 0;
-  const [ratioLeft, ratioTop] = [
-    (shadowBorderLeft - originLeft) / heightDiff,
-    (shadowBorderTop - originTop) / heightDiff,
-  ];
-
-  const area: ReturnType<typeof getShadowDirection> = [
-    ratioLeft * elementHeight,
-    ratioTop * elementHeight,
-  ];
-
-  return area;
 }
